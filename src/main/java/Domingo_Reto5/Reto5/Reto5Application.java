63f152c8f2ff92ff781ab91ec2117fd24a627163
Anime WoW AW <nicolay.corrales.mt@correo.usa.edu.co>
2021-10-31 08:10:13 -0500

    first commit

---
+++ src/main/java/Domingo_Reto5/Reto5/Reto5Application.java
@@ -0,0 +1,16 @@
+//Modelo o Entidad, Interface, Repositorio, Servicios y Controlador o Web
+
+
+package Domingo_Reto5.Reto5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto5Application.class, args);
+	}
+
+}
